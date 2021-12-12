91f6fb8184b43b6b6b5711cced9ad73d46e4b94a
Samalla1974 <94819969+Samalla1974@users.noreply.github.com>
2021-12-13 00:09:37 +0530

    Add files via upload

---
+++ snakeANDladder.java
@@ -0,0 +1,11 @@
+public class SnakeAndLadder
+{
+	public static void main(String[] args)
+	{
+		int p=0;
+		int d,i;
+		
+		d = (int)(Math.random()*6) + 1;
+		System.out.println("your die number is " + d);
+	}
+}
